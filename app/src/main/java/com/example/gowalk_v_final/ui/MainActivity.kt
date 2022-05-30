1510a76dedc5aaad9bcf5c36daed8744537e8704
Maroua Bihich <bihich.maroua@gmail.com>
2022-05-30 23:23:12 +0200

    google map partie 3

--- app/src/main/java/com/example/gowalk_v_final/ui/MainActivity.kt
+++ app/src/main/java/com/example/gowalk_v_final/ui/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.example.gowalk_v_final.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.gowalk_v_final.R
-import com.example.gowalk_v_final.db.RunDAO
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var  runDAO : RunDAO
+    private var toolbar: Toolbar? = null
+    var bottomNavigationView: BottomNavigationView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        bottomNavigationView?.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController().addOnDestinationChangedListener{
+            _,destination,_->
+            when(destination.id) {
+                R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                    bottomNavigationView?.visibility = View.VISIBLE
+                else -> bottomNavigationView?.visibility = View.GONE
+            }
+        }
 
     }
 
--- app/src/main/java/com/example/gowalk_v_final/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/example/gowalk_v_final/ui/fragments/RunFragment.kt
@@ -1,14 +1,25 @@
 package com.example.gowalk_v_final.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.gowalk_v_final.R
 import com.example.gowalk_v_final.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_run.*
 
 @AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
     private val viewModel : MainViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab.setOnClickListener{
+            findNavController().navigate(R.id.action_setupFragment_to_runFragment)
+        }
+    }
 
 }
