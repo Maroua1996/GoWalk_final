0cea6585b3d081f3324b8c82bc2af336f576c842
Maroua Bihich <bihich.maroua@gmail.com>
2022-05-30 19:04:10 +0200

    creation de l'environnement

---
+++ app/src/main/java/com/example/gowalk_v_final/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.gowalk_v_final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+
+}
