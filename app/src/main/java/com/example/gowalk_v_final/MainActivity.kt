2041abff4a471164271ddef1e957516e6a7bebee
Maroua Bihich <bihich.maroua@gmail.com>
2022-05-30 20:58:07 +0200

    première partie googlemap

--- app/src/main/java/com/example/gowalk_v_final/MainActivity.kt
+++ app/src/main/java/com/example/gowalk_v_final/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.gowalk_v_final
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.gowalk_v_final.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var  runDAO : RunDAO
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RunDao: ${runDAO.hashCode()}")
     }
 
 
---
+++ app/src/main/java/com/example/gowalk_v_final/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.gowalk_v_final.other
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
